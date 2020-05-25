d1e46f76ad9d6cba5af49e86a53b8e51e678d004
Erik Irgens <erik.t.irgens@gmail.com>
2020-05-25 14:48:40 -0700

    Boilerplate update
    Continue adding boilerplate components

---
+++ src/helpers/metric-sieve.js
@@ -0,0 +1,4 @@
+// Take input data from scraper
+// This data should be some sort of iterative data structure
+// Looking through data, extrapolate pertinent information
+// Create groupings of metric data and place them into the redux store.
--- src/helpers/scraper.js
+++ src/helpers/scraper.js
@@ -1,13 +1,4 @@
-import React from 'react'
+// This should query a given URL from the input field
+// The received HTML data should be converted into a data structure that makes most sense (string? Something?)
 
-class Scraper extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-
-        }
-    }
-
-
-}
+// Feed this data structure into the metric sieve
